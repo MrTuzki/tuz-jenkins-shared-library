879bf5298180f0c3f1bfa4e6ea1f475d948ca01f
MrTuzki <haomafan@qq.com>
2024-04-23 19:26:29 +0800

    feat: buildUtils 新增 getStageResults 方法

--- vars/buildUtils.groovy
+++ vars/buildUtils.groovy
@@ -2,7 +2,7 @@
 * 终止当前 build
 * @param reason: 终止原因
 */
-def abort(reason='') {
+def abort(String reason='') {
     currentBuild.result = 'ABORTED'
     def message
     if (reason) {
@@ -11,4 +11,27 @@ def abort(reason='') {
         message = "Aborting the build!"
     }
     error(message)
+}
+
+/**
+* 获取每个 stage 的运行结果, result 可能的返回值: SUCCESS, NOT_BUILT(等同于SKIP), FAILURE, UNKNOWN(还未执行完成)
+*/
+@NonCPS
+def Map<String, String> getStageResults() {
+    def visitor = new PipelineNodeGraphVisitor(currentBuild.rawBuild)
+
+    def results = visitor.pipelineNodes.findAll {
+        it.type == FlowNodeWrapper.NodeType.STAGE
+    }.collectEntries {
+        [it.displayName, "${it.status.result}"]
+    }
+    return results
+}
+
+def isStageSucceed(String stageName) {
+    return getStageResults().get(stageName) == 'SUCCESS'
+}
+
+def isStageFailed(String stageName) {
+    return getStageResults().get(stageName) == 'FAILURE'
 }
